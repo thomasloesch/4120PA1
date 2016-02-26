dac6ee44d8e1859e62be34a5157d569699576c9c
thomasloesch <tloesch@bgsu.edu>
2016-02-26 16:29:19 -0500

    Implemented quickSort.

--- src/QuickSortTest.java
+++ src/QuickSortTest.java
@@ -6,17 +6,36 @@ public class QuickSortTest {
 	public static void main(String[] args) {
 		ArrayList<Integer> numArray = InsertionSortTest.retriveValues(args[0]);
 
-		QuickSort(numArray, 0, numArray.size() - 1);
+		quickSort(numArray, 0, numArray.size() - 1);
 		
 		InsertionSortTest.displayValues(numArray);
 	}
 	
-	public static void QuickSort(ArrayList<Integer> A, int p, int r) {
-		
+	public static void quickSort(ArrayList<Integer> A, int p, int r) {
+		if (p < r) {
+			int q = partition(A, p, r);
+			quickSort(A, p, q - 1);
+			quickSort(A, q + 1, r);
+		}
 	}
 	
-	public static void partition(ArrayList<Integer> A, int p, int r) {
-		
+	public static int partition(ArrayList<Integer> A, int p, int r) {
+		int x = A.get(r);
+		int i = p - 1;
+		for(int j = p; j < r; j++) {
+			if(A.get(j) <= x){
+				i++;
+				swap(A, i, j);
+			}
+		}
+		swap(A, i + 1, r);
+		return i + 1;
+	}
+	
+	public static void swap(ArrayList<Integer> A, int x, int y) {
+		int temp = A.get(x);
+		A.set(x, A.get(y));
+		A.set(y, temp);
 	}
 
 }
