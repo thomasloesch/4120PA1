409ae1ccadf4c12a83ac501a87e5cf27eceffe4e
thomasloesch <tloesch@bgsu.edu>
2016-02-26 18:12:59 -0500

    Implemented MergeSort.

--- src/MergeSortTest.java
+++ src/MergeSortTest.java
@@ -11,10 +11,40 @@ public class MergeSortTest {
 	}
 
 	public static void mergeSort(ArrayList<Integer> A, int p, int r) {
-		
+		if(p < r) {
+			int q = Math.floorDiv(p + r, 2);
+			mergeSort(A, p, q);
+			mergeSort(A, q + 1, r);
+			merge(A, p, q, r);
+		}
 	}
 	
 	public static void merge(ArrayList<Integer> A, int p, int q, int r) {
+		int n1 = q - p + 1,
+			n2 = r - q,
+			L[] = new int[n1 + 1],
+			R[] = new int[n2 + 1],
+			i, j;
 		
+		for(i = 0; i < n1; i++) 
+			L[i] = A.get(p + i);
+		for(j = 0; j < n2; j++)
+			R[j] = A.get(q + j + 1);
+		
+		L[n1] = Integer.MAX_VALUE;
+		R[n2] = Integer.MAX_VALUE;
+		i = 0;
+		j = 0;
+		
+		for(int k = p; k <= r; k++) {
+			if(L[i] <= R[j]) {
+				A.set(k, L[i]);
+				i++;
+			}
+			else {
+				A.set(k, R[j]);
+				j++;
+			}
+		}
 	}
 }
