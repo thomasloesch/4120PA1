dcec3669773afcdfd1ab7fe5f2daf388c361a16d
thomasloesch <tloesch@bgsu.edu>
2016-03-04 10:02:18 -0500

    modified InsertionSortTest to output to .csv file

--- src/InsertionSortTest.java
+++ src/InsertionSortTest.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -9,16 +11,44 @@ public class InsertionSortTest {
 	public static long comparisons;
 
 	public static void main(String[] args) {
-		ArrayList<Integer> numArray = retriveValues(args[0]);
+		ArrayList<Integer> numArray;
+		try{
+			FileWriter writer = new FileWriter("output.csv");
+			
+			numArray = retriveValues(args[0]);
+			comparisons = 0;
+			insertionSortComparisons(numArray);
 		
-		
-		comparisons = 0;
-		insertionSortComparisons(numArray);
-		Date before = new Date();
-		insertionSort(numArray);
-		Date after = new Date();
-		
-		displayValues(numArray, comparisons, after.getTime() - before.getTime());
+			final int NUM_LOOPS = 100;
+			for(int i = 0; i < NUM_LOOPS; i++) {
+				numArray = retriveValues(args[0]);
+				Date before = new Date();
+				insertionSort(numArray);
+				Date after = new Date();
+				
+				displayValuesFile(numArray, comparisons, after.getTime() - before.getTime(), writer);
+			}
+			
+			writer.flush();
+			writer.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void displayValuesFile(ArrayList<Integer> A, long c, long t, FileWriter writer) {
+		try{
+			writer.append(Integer.toString(A.size()));
+			writer.append(",");
+			writer.append(Long.toString(c));
+			writer.append(",");
+			writer.append(Long.toString(t));
+			writer.append("\n");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void displayValues(ArrayList<Integer> A, long c, long t) {
@@ -54,12 +84,14 @@ public class InsertionSortTest {
 		for(int j = 1; j < A.size(); j++) {
 			key = A.get(j);
 			i = j - 1;
-			while(i >= 0 && A.get(i) > key) {
+			while(i >= 0) {
+				comparisons++;
 				if(A.get(i) > key) {
 					A.set(i + 1, A.get(i));
-					i = i - 1;
 				}
-				comparisons++;
+				else
+					break;
+				i = i - 1;
 			}
 			A.set(i + 1, key);
 		}
