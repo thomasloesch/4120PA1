0e5d28a82d4f0141d1c40c1455400f177ab5395e
thomasloesch <tloesch@bgsu.edu>
2016-03-07 16:54:26 -0500

    Added output to file and seperated comparison and time test in QuickSortTest.

--- src/QuickSortTest.java
+++ src/QuickSortTest.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -7,13 +9,28 @@ public class QuickSortTest {
 	public static void main(String[] args) {
 		ArrayList<Integer> numArray = InsertionSortTest.retriveValues(args[0]);
 
-		comparisons = 0;
-		Date before = new Date();
-		quickSort(numArray, 0, numArray.size() - 1);
-		Date after = new Date();
-		
-		InsertionSortTest.displayValues(numArray, comparisons, after.getTime() - before.getTime());
-		//InsertionSortTest.displayValues(numArray);
+		try{
+			FileWriter writer = new FileWriter("output.csv", true);
+			
+			comparisons = 0;
+			ArrayList<Integer> testArray = new ArrayList<Integer>(numArray);
+			quickSortComparison(testArray, 0, testArray.size() - 1);
+			
+			testArray = new ArrayList<Integer>(numArray);
+			Date before = new Date();
+			quickSort(testArray, 0, testArray.size() - 1);
+			Date after = new Date();
+			
+			writer.append("Quick Sort,");
+			InsertionSortTest.displayValues(numArray, comparisons, after.getTime() - before.getTime(), writer);
+			
+			writer.flush();
+			writer.close();
+		}
+		catch (IOException e) {
+			
+		}
+		//InsertionSortTest.displayValues(testArray);
 	}
 	
 	public static void quickSort(ArrayList<Integer> A, int p, int r) {
@@ -24,7 +41,28 @@ public class QuickSortTest {
 		}
 	}
 	
+	public static void quickSortComparison(ArrayList<Integer> A, int p, int r) {
+		if (p < r) {
+			int q = partitionComparison(A, p, r);
+			quickSortComparison(A, p, q - 1);
+			quickSortComparison(A, q + 1, r);
+		}
+	}
+	
 	public static int partition(ArrayList<Integer> A, int p, int r) {
+		int x = A.get(r);
+		int i = p - 1;
+		for(int j = p; j < r; j++) {
+			if(A.get(j) <= x){
+				i++;
+				swap(A, i, j);
+			}
+		}
+		swap(A, i + 1, r);
+		return i + 1;
+	}
+	
+	public static int partitionComparison(ArrayList<Integer> A, int p, int r) {
 		int x = A.get(r);
 		int i = p - 1;
 		for(int j = p; j < r; j++) {
